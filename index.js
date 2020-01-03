886d03ffc59243ffd3354dd56404d4549bacc68d
Jeretc79 <masonjeretc@gmail.com>
2020-01-03 15:52:57 -0800

    updated homework9

---
+++ index.js
@@ -0,0 +1,81 @@
+const questions = [
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "username"
+    },
+    {
+        type: "list",
+        message: "Pick your favorite color theme:",
+        name: "color",
+        choices: ["green", "blue", "pink", "red"]
+    }
+];
+
+const util = require("util");
+const puppeteer = require('puppeteer');
+const axios = require("axios");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./generateHTML");
+
+let githubURL;
+let username;
+let usercolor;
+let htmlfile;
+let html;
+
+
+function init() {
+    inquirer.prompt(questions)
+        .then(function (userInput) {
+            username = userInput.username;
+            usercolor = userInput.color; 
+            username = username.toLowerCase().trim();
+            githubURL = `https://api.github.com/users/${username}`;
+            return githubURL;
+        }, function(error){
+            console.log("Enter valid inputs!",error);
+        })
+        .then(function (githubURL) {  
+            callAxios(githubURL);
+        });
+}
+
+
+function callAxios(url) {
+    axios.get(url)
+        .then(function (response) {
+            response.data.color = usercolor; 
+            htmlfile = generateHTML(response.data);  
+            writeToFile(`${username}_profile.html`, htmlfile); 
+        })
+        .catch(function(error){
+            console.log("Make sure input a valid username of your github",error);
+        })
+}
+
+async function writeToFile(fileName, data) {
+    let writefileAsync = util.promisify(fs.writeFile);
+    await writefileAsync(fileName, data, error=>{
+        if (error) console.log(error);
+    });
+    console.log("html file is generated successfully!!!");
+    html = fs.readFileSync(`${username}_profile.html`, 'utf8');
+    convertToPDF(html, `${username}_profile.pdf`);
+}
+
+
+async function convertToPDF(html,pdfname) {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setContent(html,{waitUntil: 'networkidle0'});
+        await page.pdf({ path: pdfname, format: 'A4' },error=>{
+            if (error) console.log(error);
+        });
+        console.log("pdf file is generated successfully!!!")
+        await browser.close();
+}
+
+
+init();
